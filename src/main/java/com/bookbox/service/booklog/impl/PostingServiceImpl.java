8b8daa54b01a9e7a65342c21960a036f4fc07665
ljw122 <ljw122@LJW>
2017-10-13 14:32:09 +0900

    jw : 다시함 죄송 ㅎ

--- src/main/java/com/bookbox/service/booklog/impl/PostingServiceImpl.java
+++ src/main/java/com/bookbox/service/booklog/impl/PostingServiceImpl.java
@@ -1,7 +1,36 @@
 package com.bookbox.service.booklog.impl;
 
+import java.util.List;
+
+import com.bookbox.common.domain.Search;
 import com.bookbox.service.booklog.PostingService;
+import com.bookbox.service.domain.Posting;
+import com.bookbox.service.domain.User;
 
 public class PostingServiceImpl implements PostingService {
+
+	@Override
+	public boolean addPosting(User user, Posting posting) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Posting getPosting(User user, Posting posting) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Posting> getPostingList(Search search) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean updatePosting(User user, Posting posting) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
