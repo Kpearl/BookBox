246b9747ad53330202ca0849a07e64f9c7f6e4f5
ljw122 <ljw122@LJW>
2017-10-16 11:11:03 +0900

    JW : LogAspectJ 수정했습니다.. 죄송..

--- src/main/java/com/bookbox/common/service/TagDAO.java
+++ src/main/java/com/bookbox/common/service/TagDAO.java
@@ -1,5 +1,19 @@
 package com.bookbox.common.service;
 
+import java.util.List;
+
+import com.bookbox.common.domain.Tag;
+
 public interface TagDAO {
 
+	public int addTag(Tag tag);
+	
+	public List<Tag> getTagList(Tag tag);
+	
+	public int addTagGroup(int category, int target, List<Tag> tagList);
+	
+	public List<Tag> getTagGroupList(int category, int target);
+	
+	public int deleteTagGroup(int category, int target, List<Tag> tagList);
+	
 }
--- src/main/java/com/bookbox/common/service/TagService.java
+++ src/main/java/com/bookbox/common/service/TagService.java
@@ -1,5 +1,17 @@
 package com.bookbox.common.service;
 
+import java.util.List;
+
+import com.bookbox.common.domain.Tag;
+
 public interface TagService {
 
+	public List<Tag> getTagList(Tag tag);
+	
+	public int addTagGroup(int category, int target, List<Tag> tagList);
+	
+	public List<Tag> getTagGroupList(int category, int target);
+	
+	public int updateTagGroup(int category, int target, List<Tag> tagList);
+	
 }
